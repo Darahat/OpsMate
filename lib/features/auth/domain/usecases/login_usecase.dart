77021d21193185f1c937afecc02c05ab3ae6fbab
Didarul Alam Rahat <darahat42@gmail.com>
2025-06-08 11:15:58 +0600

    feat: login_usecase completed

---
+++ lib/features/auth/domain/usecases/login_usecase.dart
@@ -0,0 +1,49 @@
+import 'package:opsmate/features/auth/domain/entities/user.dart';
+import 'package:opsmate/features/auth/domain/repositories/auth_repository.dart';
+
+///Abstract base class for use cases in the application
+///
+/// [Type] represents the return type of the use case
+/// [Params] represents the parameters required by the use case
+abstract class UseCase<Type, Params> {
+  /// Executes the use case with the provided parameters
+  Future<Type> call(Params params);
+}
+
+/// Encapsulates the business login for user login
+///
+/// This use case handles the login operation by coordinating with
+/// the [AuthRepository] to authenticate users.
+class LoginUseCase implements UseCase<User, LoginParams> {
+  ///Creates a [LoginUseCase] instance
+  ///
+  ///Requires an [AuthRepository] implementation
+  LoginUseCase(this.repository);
+
+  ///initialising the Authrepository
+  final AuthRepository repository;
+
+  /// Executes the login operation
+  ///
+  /// Uses the provided [params] to authenticate the user
+  /// Returns a [Future] that completes with a [User] on success
+  /// Throws [ServerFailure] if authentication fails
+  @override
+  Future<User> call(LoginParams params) async {
+    return await repository.login(params.email, params.password);
+  }
+}
+
+/// Parameters required for the login operation
+class LoginParams {
+  /// creates a [LoginParams] instance
+  ///
+  /// Requires both [email] and [password] for authentication
+  LoginParams(this.email, this.password);
+
+  /// User's email address
+  final String email;
+
+  /// User's Password
+  final String password;
+}
