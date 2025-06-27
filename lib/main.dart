cf07db35a5875138dd4a9168088f758b5faed02f
Didarul Alam Rahat <darahat42@gmail.com>
2025-06-27 15:03:00 +0600

    feat: splash screen animation added, sign in login button created and functionality done

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,17 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:opsmate/app.dart';
 
 import 'features/auth/domain/user_model.dart';
+import 'firebase_options.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   // Initialize Firebase
-  await Firebase.initializeApp();
-
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   // Initialize Hive
   await Hive.initFlutter();
   Hive.registerAdapter(UserModelAdapter());
